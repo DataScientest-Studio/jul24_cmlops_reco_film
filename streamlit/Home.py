5d7f8c709a6755a60b79bbb6671682eb0584bd7d
mikhael <52800123+mikhaelbenilouz@users.noreply.github.com>
2024-11-06 15:06:01 +0100

    recupération des infos de public.users + amelioration de l'UI

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,5 +1,13 @@
 import streamlit as st
-from supabase_auth import sign_up, sign_in, sign_out
+from supabase_auth import sign_up, sign_in, sign_out, supabase
+
+
+def get_user_info(auth_id):
+    user_data = (
+        supabase.table("users").select("*").eq("authId", auth_id).single().execute()
+    )
+    return user_data.data if user_data.data else None
+
 
 st.set_page_config(
     page_title="Système de Recommandation de Films",
@@ -15,37 +23,46 @@ def main():
     if not st.session_state.authenticated:
         st.title("Bienvenue sur le Système de Recommandation de Films")
 
-        tab1, tab2 = st.tabs(["Connexion", "Inscription"])
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col2:
+            tab1, tab2 = st.tabs(["Connexion", "Inscription"])
 
-        with tab1:
-            with st.form("login_form"):
-                email = st.text_input("Email")
-                password = st.text_input("Mot de passe", type="password")
-                submit = st.form_submit_button("Se connecter")
+            with tab1:
+                with st.form("login_form"):
+                    email = st.text_input("Email")
+                    password = st.text_input("Mot de passe", type="password")
+                    submit = st.form_submit_button("Se connecter")
 
-                if submit:
-                    try:
-                        response = sign_in(email, password)
-                        st.session_state.authenticated = True
-                        st.session_state.user = response.user
-                        st.rerun()
-                    except Exception as e:
-                        st.error(f"{str(e)}")
+                    if submit:
+                        try:
+                            response = sign_in(email, password)
+                            user_info = get_user_info(response.user.id)
+                            if user_info:
+                                st.session_state.authenticated = True
+                                st.session_state.user = response.user
+                                st.session_state.user_info = user_info
+                                st.rerun()
+                            else:
+                                st.error(
+                                    "Impossible de récupérer les informations utilisateur"
+                                )
+                        except Exception as e:
+                            st.error(f"{str(e)}")
 
-        with tab2:
-            with st.form("signup_form"):
-                email = st.text_input("Email")
-                password = st.text_input("Mot de passe", type="password")
-                submit = st.form_submit_button("S'inscrire")
+            with tab2:
+                with st.form("signup_form"):
+                    email = st.text_input("Email")
+                    password = st.text_input("Mot de passe", type="password")
+                    submit = st.form_submit_button("S'inscrire")
 
-                if submit:
-                    try:
-                        sign_up(email, password)
-                        st.success(
-                            "Inscription réussie ! Veuillez vérifier votre email."
-                        )
-                    except Exception as e:
-                        st.error(f"{str(e)}")
+                    if submit:
+                        try:
+                            sign_up(email, password)
+                            st.success(
+                                "Inscription réussie ! Veuillez vérifier votre email."
+                            )
+                        except Exception as e:
+                            st.error(f"{str(e)}")
     else:
         st.switch_page("pages/1_🎬_Recommandations.py")
 
--- streamlit/pages/1_🎬_Recommandations.py
+++ streamlit/pages/1_🎬_Recommandations.py
@@ -3,13 +3,40 @@ import asyncio
 from utils import get_tmdb_ids, get_movie_info_async, display_movie_info_grid
 import requests
 
-if "authenticated" not in st.session_state or not st.session_state.authenticated:
-    st.warning("Veuillez vous connecter pour accéder à cette page.")
-    st.stop()
-
 # Charger le CSS
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+if "authenticated" not in st.session_state or not st.session_state.authenticated:
+    st.switch_page("Home.py")
+
+user_info = st.session_state.user_info
+
+genres = [
+    "(no genres listed)",
+    "Action",
+    "Adventure",
+    "Animation",
+    "Children",
+    "Comedy",
+    "Crime",
+    "Documentary",
+    "Drama",
+    "Fantasy",
+    "Film-Noir",
+    "Horror",
+    "IMAX",
+    "Musical",
+    "Mystery",
+    "Romance",
+    "Sci-Fi",
+    "Thriller",
+    "War",
+    "Western",
+]
+
+# Construire la user_matrix à partir des valeurs de genres
+user_matrix = ",".join(str(user_info[genre]) for genre in genres)
 
 
 async def main(movie_ids):
@@ -24,39 +51,23 @@ async def main(movie_ids):
     await display_movie_info_grid(movies_info)
 
 
-# TODO remove this when Auth is implemented
-# user_matrix = "0.0,0.37714285714285717,0.41714285714285715,0.05714285714285714,0.10857142857142857,0.2342857142857143,0.12,0.0,0.24571428571428572,0.3942857142857143,0.0,0.2571428571428571,0.011428571428571429,0.017142857142857144,0.10285714285714286,0.06285714285714286,0.22857142857142856,0.24,0.05142857142857143,0.022857142857142857"
-# user_matrix = "0.0,0.3114754098360656,0.2786885245901639,0.01639344262295082,0.01639344262295082,0.16393442622950818,0.01639344262295082,0.0,0.3114754098360656,0.01639344262295082,0.01639344262295082,0.29508196721311475,0.01639344262295082,0.03278688524590164,0.06557377049180328,0.09836065573770492,0.3770491803278688,0.3114754098360656,0.06557377049180328,0.03278688524590164"
-# user_matrix = "0.0,0.32620320855614976,0.26737967914438504,0.0213903743315508,0.053475935828877004,0.27807486631016043,0.11229946524064172,0.0053475935828877,0.31016042780748665,0.10695187165775401,0.0053475935828877,0.1711229946524064,0.0,0.03208556149732621,0.058823529411764705,0.0855614973262032,0.49732620320855614,0.26737967914438504,0.03208556149732621,0.016042780748663103"
-# user_matrix = "0.0,0.4642857142857143,0.21428571428571427,0.07142857142857142,0.14285714285714285,0.39285714285714285,0.21428571428571427,0.0,0.2857142857142857,0.10714285714285714,0.0,0.0,0.0,0.07142857142857142,0.10714285714285714,0.14285714285714285,0.17857142857142858,0.4642857142857143,0.03571428571428571,0.03571428571428571"
-user_matrix = "0.0,0.2727272727272727,0.3181818181818182,0.09090909090909091,0.16666666666666666,0.36363636363636365,0.10606060606060606,0.0,0.4090909090909091,0.16666666666666666,0.0,0.015151515151515152,0.045454545454545456,0.12121212121212122,0.030303030303030304,0.24242424242424243,0.15151515151515152,0.22727272727272727,0.015151515151515152,0.030303030303030304"
-# user_matrix = "0.0,0.07692307692307693,0.0,0.0,0.0,0.38461538461538464,0.15384615384615385,0.038461538461538464,0.4230769230769231,0.038461538461538464,0.0,0.11538461538461539,0.0,0.0,0.0,0.11538461538461539,0.15384615384615385,0.38461538461538464,0.07692307692307693,0.038461538461538464"
+st.title("Recommandations de Films basées sur votre Historique")
 
-st.title("Recommandations de Films")
+if user_matrix:
+    api_url = "http://api_predict:8002/recommend"
+    user_input = {"genres": user_matrix}
 
-with st.expander("Afficher la matrice utilisateur"):
-    st.write(user_matrix)
+    response = requests.post(api_url, json=user_input)
 
-if st.button("Obtenir des recommandations"):
-    if user_matrix:
-        api_url = "http://api_predict:8002/recommend"
-        user_input = {"genres": user_matrix}
+    if response.status_code == 200:
+        recommendations = response.json()
 
-        response = requests.post(api_url, json=user_input)
-
-        if response.status_code == 200:
-            recommendations = response.json()
-            st.write("Voici vos recommandations de films :")
-
-            if (
-                "recommendations" in recommendations
-                and recommendations["recommendations"]
-            ):
-                movie_ids = recommendations["recommendations"][0]
-                asyncio.run(main(movie_ids))
-            else:
-                st.error("Aucune recommandation disponible.")
+        if "recommendations" in recommendations and recommendations["recommendations"]:
+            movie_ids = recommendations["recommendations"][0]
+            asyncio.run(main(movie_ids))
         else:
-            st.error("Erreur lors de la récupération des recommandations.")
+            st.error("Aucune recommandation disponible.")
     else:
-        st.warning("Veuillez sélectionner au moins un genre.")
+        st.error("Erreur lors de la récupération des recommandations.")
+else:
+    st.warning("Veuillez sélectionner au moins un genre.")
