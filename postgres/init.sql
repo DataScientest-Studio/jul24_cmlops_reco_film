c2132db0419e712afef98f4b78ba6977c4618ce6
AntoinePELAMOURGUES <antoinepela@gmail.com>
2024-11-14 01:46:43 +0400

    :heavy_plus_sign: Add & config new database

--- postgres/init.sql
+++ postgres/init.sql
@@ -4,33 +4,33 @@ CREATE TABLE IF NOT EXISTS users (
     userId SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    hached_password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL
 );
 
 -- Charger les données à partir du fichier CSV
-COPY users(userId, username, email, hached_password) FROM '/docker-entrypoint-initdb.d/users.csv' DELIMITER ',' CSV HEADER;
+COPY users(userId, username, email, password) FROM '/docker-entrypoint-initdb.d/users.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS movies (
     movieId SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    genres VARCHAR(200),
+    title VARCHAR(200) NOT NULL,
+    genres TEXT,
     year INT
 );
 
 -- Charger les données à partir du fichier CSV
-COPY movies(movieId, title, genres) FROM '/docker-entrypoint-initdb.d/processed_movies.csv' DELIMITER ',' CSV HEADER;
+COPY movies(movieId, title, genres, year) FROM '/docker-entrypoint-initdb.d/processed_movies.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS ratings (
     id SERIAL PRIMARY KEY,
     userId INT REFERENCES users(userId),
     movieId INT REFERENCES movies(movieId),
     rating FLOAT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    timestamp INT,
     bayesian_mean FLOAT NOT NULL
 );
 
 -- Charger les données à partir du fichier CSV
-COPY ratings(userId, movieId, rating, created_at, bayesian_mean) FROM '/docker-entrypoint-initdb.d/processed_ratings.csv' DELIMITER ',' CSV HEADER;
+COPY ratings(userId, movieId, rating, timestamp, bayesian_mean) FROM '/docker-entrypoint-initdb.d/processed_ratings.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS links (
     id SERIAL PRIMARY KEY,
